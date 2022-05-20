47ba794e21f2335688767ef75d1ca7b905dfbdb4
Angel Cowo <angeljcowo@gmail.com>
2022-05-20 09:10:21 -0600

    Added failsafe when question is done

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,22 @@ class MyApp extends StatefulWidget {
 
 //data is not reset here
 class _MyAppState extends State<MyApp> {
+  final questions = [
+    //map-> key=>value pair
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': ['Black', 'Red', 'Green', 'White'],
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': ['Rabit', 'Snake', 'Elephant', 'Lion'],
+    },
+    {
+      'questionText': 'What\'s your favorite person?',
+      'answers': ['skrt', 'myself', 'joey', 'YOOOO'],
+    },
+  ];
+
   var _questionIndex = 0;
 
   //answerQuestionFuction
@@ -26,43 +42,35 @@ class _MyAppState extends State<MyApp> {
       _questionIndex++;
     });
     print(_questionIndex);
+
+    if (_questionIndex < questions.length) {
+      print('We have more Questions!');
+    }
   }
 
   Widget build(BuildContext context) {
-    var questions = [
-      //map-> key=>value pair
-      {
-        'questionText': 'What\'s your favorite color?',
-        'answers': ['Black', 'Red', 'Green', 'White'],
-      },
-      {
-        'questionText': 'What\'s your favorite animal?',
-        'answers': ['Rabit', 'Snake', 'Elephant', 'Lion'],
-      },
-      {
-        'questionText': 'What\'s your favorite person?',
-        'answers': ['Perla', 'Perla', 'Perla', 'Definately Perla'],
-      },
-    ];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Column(
-          children: <Widget>[
-            Question(
-              questions[_questionIndex]['questionText'] as String,
-            ),
-            //the elipses adds the values of a list to a list, so
-            //a list isnt being added to a list
-            ...(questions[_questionIndex]['answers'] as List<String>)
-                .map((answer) {
-              return Answer(_answerQuestion, answer);
-            }).toList()
-          ],
-        ),
+        body: _questionIndex < questions.length
+            ? Column(
+                children: <Widget>[
+                  Question(
+                    questions[_questionIndex]['questionText'] as String,
+                  ),
+                  //the elipses adds the values of a list to a list, so
+                  //a list isnt being added to a list
+                  ...(questions[_questionIndex]['answers'] as List<String>)
+                      .map((answer) {
+                    return Answer(_answerQuestion, answer);
+                  }).toList()
+                ],
+              )
+            : Center(
+                child: Text('You did it!'),
+              ),
       ),
     );
   }
