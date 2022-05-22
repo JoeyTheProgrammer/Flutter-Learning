b896590b4bbb65775deee270ef78a804b04b60d0
Angel Cowo <angeljcowo@gmail.com>
2022-05-22 15:42:15 -0600

    adding styling to the output static data

--- lib/main.dart
+++ lib/main.dart
@@ -54,14 +54,43 @@ class MyHomePage extends StatelessWidget {
               return Card(
                 child: Row(children: <Widget>[
                   Container(
+                    margin: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 15,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
+                      ),
+                    ),
+                    padding: EdgeInsets.all(10),
                     child: Text(
                       tx.amount.toString(),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+                      ),
                     ),
                   ),
-                  Column(children: <Widget>[
-                    Text(tx.title),
-                    Text(tx.date.toString()),
-                  ])
+                  Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          tx.title,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                        Text(
+                          tx.date.toString(),
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ])
                 ]),
               );
             }).toList(),
