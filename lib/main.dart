f3bf15a5618169d1fbe573b2d4664d4803d34638
Angel Cowo <angeljcowo@gmail.com>
2022-06-03 14:03:38 -0600

    added your products screen

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:provider/provider.dart';
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
 import './providers/products.dart';
+import './screens/user_products_Screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -38,7 +39,8 @@ class MyApp extends StatelessWidget {
         routes: {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           CartScreen.routeName: (ctx) => CartScreen(),
-          OrdersScreen.routeName: (ctx) => OrdersScreen()
+          OrdersScreen.routeName: (ctx) => OrdersScreen(),
+          UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
         },
       ),
     );
