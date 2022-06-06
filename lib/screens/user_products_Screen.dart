dbc49ecfbbb28d96263d73bd6e45a7171f2bc0d3
Angel Cowo <angeljcowo@gmail.com>
2022-06-06 15:30:57 -0600

    finalizing this module

--- lib/screens/user_products_Screen.dart
+++ lib/screens/user_products_Screen.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import '../providers/products.dart';
 import '../widgets/user_product_item.dart';
 import '../widgets/app_drawer.dart';
+import './edit_product_screen.dart';
 
 class UserProductsScreen extends StatelessWidget {
   static const routeName = '/user-products';
@@ -17,25 +18,29 @@ class UserProductsScreen extends StatelessWidget {
         actions: [
           IconButton(
             icon: const Icon(Icons.add),
-            onPressed: () {},
+            onPressed: () {
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
+            },
           ),
         ],
       ),
       drawer: AppDrawer(),
       body: Padding(
-          padding: EdgeInsets.all(8),
-          child: ListView.builder(
-            itemBuilder: (context, i) => Column(
-              children: [
-                UserProductItem(
-                  productsData.items[i].title,
-                  productsData.items[i].imageUrl,
-                ),
-                Divider(),
-              ],
-            ),
-            itemCount: productsData.items.length,
-          )),
+        padding: EdgeInsets.all(8),
+        child: ListView.builder(
+          itemBuilder: (context, i) => Column(
+            children: [
+              UserProductItem(
+                productsData.items[i].id,
+                productsData.items[i].title,
+                productsData.items[i].imageUrl,
+              ),
+              Divider(),
+            ],
+          ),
+          itemCount: productsData.items.length,
+        ),
+      ),
     );
   }
 }
